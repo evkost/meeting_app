1e7d708f01957be10a7be8c6b582d82ee8c8bd96
kostoev05 <kostoev.05@mail.ru>
2023-02-27 21:56:06 +0300

    Added expand more button

--- app/src/main/java/com/evkost/meetingapp/ui/meeting/MeetingScreen.kt
+++ app/src/main/java/com/evkost/meetingapp/ui/meeting/MeetingScreen.kt
@@ -1,15 +1,20 @@
 package com.evkost.meetingapp.ui.meeting
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
@@ -20,21 +25,21 @@ import com.evkost.meetingapp.model.MemberType
 import com.evkost.meetingapp.ui.meeting.component.*
 import kotlinx.collections.immutable.persistentListOf
 
-//@Preview(showBackground = true)
-//@Composable
-//fun MeetingScreenPreview() {
-//    MeetingScreen(
-//        uiState = MeetingState.Done(
-//            Member(id = 1, false, true, "", "", MemberType.ANOTHER_USER),
-//            persistentListOf()
-//        ),
-//        onToggleVideo = {},
-//        onToggleVoice = {},
-//        onShowMembersBottomSheet = {},
-//        onFinishActivity = {},
-//        onStartSmsActivity = {}
-//    )
-//}
+@Preview(showBackground = true)
+@Composable
+fun MeetingScreenPreview() {
+    MeetingScreen(
+        uiState = MeetingState.Done(
+            Member(id = 1, false, true, "", "", MemberType.ANOTHER_USER),
+            persistentListOf()
+        ),
+        onToggleVideo = {},
+        onToggleVoice = {},
+        onShowMembersBottomSheet = {},
+        onFinishActivity = {},
+        onStartSmsActivity = {}
+    )
+}
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -81,39 +86,60 @@ fun MeetingScreen(
     ConstraintLayout(
         modifier = modifier,
     ) {
-        val (topButtonsRow, membersGrid, bottomButtonsRow) = createRefs()
+        val (topButtonsRow, membersList, bottomSheetSwipe, bottomButtonsRow) = createRefs()
 
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically,
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
                 .padding(horizontal = 16.dp, vertical = 5.dp)
                 .constrainAs(topButtonsRow) {
                     top.linkTo(parent.top)
                 }
         ) {
-            ChatButton(
-                onClick = onStartSmsActivity
-            )
+            Box(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                IconButton(
+                    onClick = { },
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .height(30.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.ExpandMore,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                }
+            }
 
-            UsersButton(
-                userCount = members.size,
-                onClick = onShowMembersBottomSheet
-            )
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                ChatButton(
+                    onClick = onStartSmsActivity
+                )
 
-            ReplaceUsersButton(
-                onClick = { isReversed = !isReversed }
-            )
+                UsersButton(
+                    userCount = members.size,
+                    onClick = onShowMembersBottomSheet
+                )
+
+                ReplaceUsersButton(
+                    onClick = { isReversed = !isReversed }
+                )
+            }
         }
 
         LazyColumn(
             modifier = modifier
                 .padding(horizontal = 5.dp)
-                .constrainAs(membersGrid) {
+                .constrainAs(membersList) {
                     linkTo(
                         top = topButtonsRow.bottom,
-                        bottom = bottomButtonsRow.top,
+                        bottom = bottomSheetSwipe.top,
                         bottomMargin = 16.dp
                     )
                     height = Dimension.fillToConstraints
@@ -129,6 +155,21 @@ fun MeetingScreen(
                 )
             }
         }
+
+        Box(
+            modifier = Modifier
+                .width(40.dp)
+                .height(4.dp)
+                .clip(RoundedCornerShape(100))
+                .background(MaterialTheme.colorScheme.onBackground)
+                .constrainAs(bottomSheetSwipe) {
+                    linkTo(
+                        start = parent.start,
+                        end = parent.end,
+                    )
+                    bottom.linkTo(bottomButtonsRow.top, margin = 15.dp)
+                }
+        )
 
         Row(
             horizontalArrangement = Arrangement.SpaceEvenly,
